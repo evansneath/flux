6a8b6d2323699cdfc583e8dca231888c9d24c507
Evan Sneath <evansneath@gmail.com>
2011-08-21 11:34:38 +0200

    Modified snd_test to provide a primative pass-through signal

--- src/core/tests/snd_test.py
+++ src/core/tests/snd_test.py
@@ -12,13 +12,17 @@ def main():
     in_signal = sndobj.SndRTIO(2, sndobj.SND_INPUT)
     out_signal = sndobj.SndRTIO(2, sndobj.SND_OUTPUT)
     
-    # Pass input signal through to the output
-    out_signal.SetOutput(1, in_signal)
+    snd = sndobj.SndIn(in_signal)
     
+    out_signal.SetOutput(1, snd)
+    out_signal.SetOutput(2, snd)
+   
     # Setup thread object
-    thread = sndobj.SndThread() 
+    thread = sndobj.SndThread()
+    
     thread.AddObj(in_signal, sndobj.SNDIO_IN) 
     thread.AddObj(out_signal, sndobj.SNDIO_OUT)
+    thread.AddObj(snd)
     
     # Begin processing thread. Run for 30 seconds
     print('Start: Test thread')
