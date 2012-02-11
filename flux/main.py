a29d7c95c4ff6ed1a674f136478f78f56725aab5
AJ Alt <ctrl.alt.aj@gmail.com>
2012-02-10 19:41:57 -0500

    Added drag and drop effects rearranging and updated some icons.

--- flux/main.py
+++ flux/main.py
@@ -23,6 +23,9 @@ class FlowLayout(QtGui.QLayout):
         item = self.takeAt(0)
         while item:
             item = self.takeAt(0)
+            
+    def widget_index(self, widget):
+        return [item.widget() for item in self.itemList].index(widget)
 
     def addItem(self, item):
         self.itemList.append(item)
@@ -35,7 +38,7 @@ class FlowLayout(QtGui.QLayout):
             return self.itemList[index]
 
         return None
-
+    
     def takeAt(self, index):
         if index >= 0 and index < len(self.itemList):
             return self.itemList.pop(index)
@@ -95,20 +98,35 @@ class FlowLayout(QtGui.QLayout):
         
         
 class FluxCentralWidget(QtGui.QTabWidget):
+    widgets_changed = QtCore.Signal()
+    
+    _drag_indicator_padding = QtCore.QPoint(0, 13)
+    
     def __init__(self):
         super(FluxCentralWidget, self).__init__()
         
-        #self.setDocumentMode(True)
+        self.drag_widget = None
+        
+        self.drag_indicator = QtGui.QLabel(self)
+        self.drag_indicator.setPixmap(QtGui.QPixmap('res/icons/drag_indicator.png'))
+        self.drag_indicator.hide()
+        
+        self.setAcceptDrops(True)
         
         self.add_tab_button = QtGui.QPushButton(QtGui.QIcon('res/icons/tab_add.png'), '')
-        self.add_tab_button.pressed.connect(self.add_tab)
+        self.add_tab_button.clicked.connect(self.add_tab)
         self.add_tab_button.setObjectName('add_tab_button')
         
         self.setCornerWidget(self.add_tab_button, QtCore.Qt.BottomRightCorner)
         
         self.tabCloseRequested.connect(self.remove_tab)
+        self.currentChanged.connect(self.current_changed_event)
         
         self.add_tab()
+        
+    def current_changed_event(self, index):
+        if index >= 0:
+            self.widgets_changed.emit()
     
     def add_tab(self):
         tab = QtGui.QWidget()
@@ -136,7 +154,6 @@ class FluxCentralWidget(QtGui.QTabWidget):
                 self.setCurrentIndex(self.count() - 1)
                 
     def rename_tab(self):
-        print self.count()
         if self.count():
             text, ok = QtGui.QInputDialog.getText(self, 'Rename preset', 'Name:',
                                 QtGui.QLineEdit.Normal, self.tabText(self.currentIndex()))
@@ -146,14 +163,87 @@ class FluxCentralWidget(QtGui.QTabWidget):
     def add_widget(self, widget):
         if self.count():
             self.currentWidget().layout.addWidget(widget)
+            self.widgets_changed.emit()
         else:
             print "Warning: can't add widget if there are no tabs"
+            
+    def move_widget(self, index, new_index):
+        l = self.currentWidget().layout.itemList
+        widget = l.pop(index)
+        l.insert(new_index, widget)
+        self.currentWidget().layout.invalidate()
+        self.widgets_changed.emit()
         
     def remove_widget(self, widget):
         if self.count():
             self.currentWidget().layout.removeWidget(widget)
+            widget.hide()
+            self.widgets_changed.emit()
         else:
             print "Warning: can't remove widget if there are no tabs"
+            
+    def _effect_widget_at(self, pos):
+        widget = self.childAt(pos)
+        if isinstance(widget, QtGui.QLabel):
+            widget = widget.parentWidget()
+        if isinstance(widget, EffectWidgetTitleBar):
+            widget = widget.parentWidget()
+        if isinstance(widget, EffectWidget):
+            return widget
+        return None
+        
+    def mousePressEvent(self, event):
+        widget = self._effect_widget_at(event.pos())
+        if widget is not None:
+            self.drag_widget = widget
+            
+            pixmap = QtGui.QPixmap.grabWidget(widget)
+            
+            mime_data = QtCore.QMimeData()
+            mime_data.setData('application/effect_widget', '')
+            drag = QtGui.QDrag(self)
+            drag.setPixmap(pixmap)
+            drag.setMimeData(mime_data)
+            drag.start(QtCore.Qt.MoveAction)
+        
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasFormat('application/effect_widget'):
+            event.accept()
+        else:
+            event.ignore()
+            
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasFormat('application/effect_widget'):
+            widget = self._effect_widget_at(event.pos())
+            if widget is None:
+                #place the indicator to the right of the last widget
+                widget = self.currentWidget().layout.itemList[-1].widget()
+                pos = widget.pos() + self._drag_indicator_padding + QtCore.QPoint(widget.width(), 0)
+            else:
+                pos = widget.pos() + self._drag_indicator_padding
+            
+            self.drag_indicator.move(pos)
+            self.drag_indicator.show()
+            
+            event.accept()
+        else:
+            event.ignore()
+            
+    def dropEvent(self, event):
+        layout = self.currentWidget().layout
+        original_index = layout.widget_index(self.drag_widget)
+        widget = self._effect_widget_at(event.pos())
+        if widget is not None:
+            new_index = layout.widget_index(widget)
+        else:
+            new_index = layout.count()
+            
+        self.drag_indicator.hide()
+        self.move_widget(original_index, new_index)
+        
+        event.setDropAction(QtCore.Qt.MoveAction)
+        event.accept()
+        self.drag_widget = None
             
         
 class EffectWidgetTitleBar(QtGui.QFrame):
@@ -166,7 +256,7 @@ class EffectWidgetTitleBar(QtGui.QFrame):
         self.layout.addWidget(self.label, alignment=QtCore.Qt.AlignLeft)
         
         style = app.style()
-        close_icon = QtGui.QIcon('res/icons/tab_close_hover.png')
+        close_icon = QtGui.QIcon('res/icons/tab_close.png')
         self.exit_btn = QtGui.QPushButton(close_icon, '')
         self.exit_btn.setObjectName('effect_exit_btn')
         self.exit_btn.setFlat(True)
@@ -215,6 +305,8 @@ class EffectWidget(QtGui.QFrame):
             param.value = param.type(ratio * (param.maximum - param.minimum) + param.minimum)
         return update_paramater
    
+    
+   
 class TempoWidget(QtGui.QWidget):
     def __init__(self):
         super(TempoWidget, self).__init__()
@@ -235,7 +327,7 @@ class TempoWidget(QtGui.QWidget):
         
         #self.button = QtGui.QPushButton(QtGui.QIcon('res/icons/time_go.png'), '')
         #self.layout.addWidget(self.button)
-        #self.button.pressed.connect(self.update_tempo)
+        #self.button.clicked.connect(self.update_tempo)
         
         self.tempo_times = collections.deque(maxlen=5)
         
@@ -257,8 +349,6 @@ class TempoWidget(QtGui.QWidget):
                 prev = elem
             interval = s / float(len(self.tempo_times) - 1)
             self.bpm_entry.setText(str(int(60/interval))) #60/(s per beat) = bpm
-        
-        
                 
 class FluxWindow(QtGui.QMainWindow):
     def __init__(self, app):
@@ -294,13 +384,13 @@ class FluxWindow(QtGui.QMainWindow):
         self.toolbar.setFloatable(False)
         self.toolbar.setMovable(False)
         
-        self.toolbar.addAction(QtGui.QIcon('res/icons/control_play.png'), 'Play', self.play_btn_action)
-        self.toolbar.addAction(QtGui.QIcon('res/icons/control_pause.png'), 'Bypass Effects', self.pause_btn_action)
-        self.toolbar.addAction(QtGui.QIcon('res/icons/control_stop.png'), 'Stop', self.stop_btn_action)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/control_play.png'), 'Play', self.play_btn_event)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/control_pause.png'), 'Bypass Effects', self.pause_btn_event)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/control_stop.png'), 'Stop', self.stop_btn_event)
         self.toolbar.addSeparator()
-        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_left.png'), 'Next Preset', self.tab_left_action)
-        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_right.png'), 'Previous Preset', self.tab_right_action)
-        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_edit.png'), 'Rename Preset', self.rename_tab_action)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_left.png'), 'Next Preset', self.tab_left_event)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_right.png'), 'Previous Preset', self.tab_right_event)
+        self.toolbar.addAction(QtGui.QIcon('res/icons/tab_edit.png'), 'Rename Preset', self.rename_tab_event)
         self.toolbar.addSeparator()
         self.toolbar.addWidget(self.tempo_widget)
         self.toolbar.addAction(QtGui.QIcon('res/icons/time_go.png'), 'Tap Tempo', self.tempo_widget.update_tempo)
@@ -312,30 +402,30 @@ class FluxWindow(QtGui.QMainWindow):
         self.central_widget.setMovable(True)
         self.central_widget.setTabsClosable (True)
         self.setCentralWidget(self.central_widget)
-        self.central_widget.currentChanged.connect(self.update_audio_path)
+        self.central_widget.widgets_changed.connect(self.update_audio_path)
         
     def sizeHint(self):
         return QtCore.QSize(600, 400)
 
-    def play_btn_action(self):
+    def play_btn_event(self):
         self.audio_path.start()
         
-    def pause_btn_action(self):
+    def pause_btn_event(self):
         self.audio_path.processing_enabled = False
         
-    def stop_btn_action(self):
+    def stop_btn_event(self):
         self.audio_path.stop()
         
-    def add_tab_action(self):
+    def add_tab_event(self):
         self.central_widget.add_tab()
         
-    def tab_left_action(self):
+    def tab_left_event(self):
         self.central_widget.select_prev_tab()
     
-    def tab_right_action(self):
+    def tab_right_event(self):
         self.central_widget.select_next_tab()
         
-    def rename_tab_action(self):
+    def rename_tab_event(self):
         self.central_widget.rename_tab()
         
     def effect_list_item_selected(self, list_item):
@@ -345,21 +435,19 @@ class FluxWindow(QtGui.QMainWindow):
                 self.audio_path.effects.append(effect)
                 widget = EffectWidget(effect)
                 self.central_widget.add_widget(widget)
-                widget.title_bar.exit_btn.pressed.connect(lambda: self.remove_effect_widget(widget))
+                widget.title_bar.exit_btn.clicked.connect(lambda: self.remove_effect_widget(widget))
                 return
             
     def remove_effect_widget(self, widget):
-        self.central_widget.remove_widget(widget)
-        widget.hide()
         try:
             self.audio_path.effects.remove(widget.effect)
         except ValueError as err:
             print 'Info: effect already removed'
+        self.central_widget.remove_widget(widget)
             
-    def update_audio_path(self, index):
-        if index >= 0:
-            panel = self.central_widget.currentWidget()
-            self.audio_path.effects = [i.widget().effect for i in panel.layout.itemList]
+    def update_audio_path(self):
+        panel = self.central_widget.currentWidget()
+        self.audio_path.effects = [i.widget().effect for i in panel.layout.itemList]
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
