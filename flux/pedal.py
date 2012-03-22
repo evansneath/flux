138a79aac673771f20f884b77ab0dcf186cda25a
pmusgrave <ps.musgrave@gmail.com>
2012-03-22 13:57:33 -0400

    Added long presses for the action button. 2 sec presses are long presses

--- flux/pedal.py
+++ flux/pedal.py
@@ -1,10 +1,12 @@
 import serial
+import time
 from PySide import QtCore
 
 class PedalThread(QtCore.QThread):    
     left_clicked = QtCore.Signal()
     right_clicked = QtCore.Signal()
     action_clicked = QtCore.Signal()
+    action_longpress = QtCore.Signal()
     
     def __init__(self):
         super(PedalThread, self).__init__()
@@ -17,12 +19,15 @@ class PedalThread(QtCore.QThread):
     def run(self):
         while True: 
             line = self.connection.readline().rstrip()
-            if line == 'L':
+            if line == 'L1':
                 self.left_clicked.emit()
-            elif line == 'R':
+            elif line == 'R1':
                 self.right_clicked.emit()
-            elif line == 'E':
-                self.action_clicked.emit()
-
-
-        
+            elif line == 'E1':
+                t1 = time.clock()
+            elif line == 'E0':
+                t2 = time.clock()
+                if (t2 - t1) < 2:
+                    self.action_clicked.emit()
+                elif (t2 - t1) > 2:
+                    self.action_longpress.emit()
